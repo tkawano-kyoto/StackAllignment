aecc09dd974bb4910ea9fe7470dafe269a3355c5
Tamotsu Kawano <t.k.kyoto@gmail.com>
2023-09-23 08:55:53 +0900

    Initial Commit

---
+++ StackAllignment/StackAllignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackAllignmentApp.swift
+//  StackAllignment
+//
+//  Created by 河野保 on 2023/09/23.
+//
+
+import SwiftUI
+
+@main
+struct StackAllignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
